39e16d7e18a61a77aa17d79159098f47c1f2702e
Bogdan POPA <bpopa@pentalog.fr>
2017-11-08 13:45:59 +0200

    Although not merging to master, we could also:  - Use a bijective function to convert the id to a shortcode  - Conversion will be from base 10 to base 62  - We could easily use the generated shortcode to fetch original url

--- app/models/short_link.rb
+++ app/models/short_link.rb
@@ -5,7 +5,7 @@ class ShortLink < ApplicationRecord
   validates :shortcode, uniqueness: true
   validates :shortcode, format: { with: /\A[0-9a-zA-Z_]{6}\z/i }, allow_nil: true
 
-  ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_".split(//)
+  ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split(//)
 
   before_create { generate_shortcode(:shortcode) }
   after_create :url_encode_date
@@ -13,8 +13,21 @@ class ShortLink < ApplicationRecord
   def generate_shortcode(shortcode)
     return if valid_preferential_shortcode?(shortcode)
     begin
-      self[shortcode] = (0...6).map { ALPHABET[rand(ALPHABET.length)] }.join
+      self[shortcode] = shortlink_encode
     end while shortcode_exists?(shortcode)
+  end
+
+  def shortlink_encode
+    encoded_shortcode = []
+    base = ALPHABET.length
+    uniq_identifier = ShortLink.last.id + 1
+
+    while uniq_identifier > 0
+      encoded_shortcode << ALPHABET[uniq_identifier.modulo(base)]
+      uniq_identifier = uniq_identifier/base
+    end
+    encoded_shortcode << 0 while encoded_shortcode.length < 6
+    encoded_shortcode.join('').reverse
   end
 
   def valid_preferential_shortcode?(shortcode)
